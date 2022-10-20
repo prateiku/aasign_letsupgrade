8b3f208eccf2893700a63d4bf78180fef8c93e08
Pratik <79096422+prateiku@users.noreply.github.com>
2022-10-20 16:48:54 +0530

    Create cpp_assign.cpp

---
+++ cpp_assign.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1, num2;
+
+    cout << "Input Number1 and Number2 :";
+    cin >> num1 >> num2;
+
+    if (num1 == num2){
+        cout << "both are equal"<<endl;
+    }else{
+        cout << "both nos. are not equal"<<endl;
+    }
+}
